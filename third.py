d9438cb3015d8d755777e78587686d4ce77b2d44
Sakib Ahamed <sakib2ahamed@gmail.com>
2022-11-06 23:32:26 +0530

    python program

---
+++ third.py
@@ -0,0 +1,9 @@
+#using conditions
+marks = int(input('enter the marks'))
+print(type(marks))
+
+#print(type(marks))
+if marks>=18:
+    print("the student has passed the exam")
+else:
+    print("the student has not passed the exam")
