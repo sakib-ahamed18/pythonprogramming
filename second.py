9b500ac361bc63c3f7eac99fc37f82d31a191bcf
Sakib Ahamed <sakib2ahamed@gmail.com>
2022-11-06 23:27:49 +0530

    python program

---
+++ second.py
@@ -0,0 +1,19 @@
+#problem
+age = input("enter your age")
+print( "i am",age,"years old")
+#another wat is
+age = 12
+print("I am" + str(age) + "years old")
+
+#calculating the seconds in a week
+secondsinaday = 60*60*24
+print(secondsinaday)
+
+secondsinaweek = secondsinaday*7
+print(secondsinaweek)
+
+secondsinamonth = secondsinaday*30
+print(secondsinamonth)
+
+secondsinayear = secondsinaday*365
+print(secondsinayear)
